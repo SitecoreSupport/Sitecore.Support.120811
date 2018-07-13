4936d501f77fae80a17d628fcbbf5875cd9f1495
AnnaAkimova <aak@sitecore.net>
2018-07-13 09:54:31 +0300

    Fix commit

--- src/Sitecore.Support.120811/FieldEditor.cs
+++ src/Sitecore.Support.120811/FieldEditor.cs
@@ -4,34 +4,46 @@
   using Sitecore.Data;
   using Sitecore.Data.Items;
   using Sitecore.Diagnostics;
-  using Sitecore.Shell.Applications.WebEdit;
+  using Sitecore.Shell.Applications.WebEdit.Commands;
   using Sitecore.Shell.Framework.Commands;
   using Sitecore.Text;
   using Sitecore.Web.UI.Sheer;
   using System;
   using System.Collections.Generic;
   using System.Collections.Specialized;
-  using Sitecore.Shell.Applications.WebEdit.Commands;
+  using System.Web;
+  using System.Web.UI;
 
-[Serializable]
-  public class FieldEditor : FieldEditorCommand
+  [Serializable]
+  public class FieldEditor : WebEditCommand
   {
-    protected override PageEditFieldEditorOptions GetOptions(ClientPipelineArgs args, NameValueCollection form)
+    public override void Execute(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      if (context.Items.Length >= 1)
+      {
+        ClientPipelineArgs args = new ClientPipelineArgs(context.Parameters);
+        args.Parameters.Add("uri", context.Items[0].Uri.ToString());
+        Context.ClientPage.Start(this, "StartFieldEditor", args);
+      }
+    }
+
+    protected PageEditFieldEditorOptions GetOptions(ClientPipelineArgs args, NameValueCollection form)
     {
       List<FieldDescriptor> fields = new List<FieldDescriptor>();
       Item item = Database.GetItem(ItemUri.Parse(args.Parameters["uri"]));
       Assert.IsNotNull(item, "item");
-      string str = args.Parameters["fields"];
-      Assert.IsNotNullOrEmpty(str, "Field Editor command expects 'fields' parameter");
-      string str2 = args.Parameters["command"];
-      Assert.IsNotNullOrEmpty(str2, "Field Editor command expects 'command' parameter");
-      Item item2 = Client.CoreDatabase.GetItem(str2);
+      string text1 = args.Parameters["fields"];
+      Assert.IsNotNullOrEmpty(text1, "Field Editor command expects 'fields' parameter");
+      string str = args.Parameters["command"];
+      Assert.IsNotNullOrEmpty(str, "Field Editor command expects 'command' parameter");
+      Item item2 = Client.CoreDatabase.GetItem(str);
       Assert.IsNotNull(item2, "command item");
-      foreach (string str3 in new ListString(str))
+      foreach (string str2 in new ListString(text1))
       {
-        if (item.Fields[str3] != null)
+        if (item.Fields[str2] != null)
         {
-          fields.Add(new FieldDescriptor(item, str3));
+          fields.Add(new FieldDescriptor(item, str2));
         }
       }
       return new PageEditFieldEditorOptions(form, fields)
@@ -44,11 +56,36 @@
     public override CommandState QueryState(CommandContext context)
     {
       Assert.ArgumentNotNull(context, "context");
-      if (((context.Items.Length > 0) && (context.Items[0] != null)) && !context.Items[0].Access.CanWrite())
+      if (((context.Items.Length != 0) && (context.Items[0] != null)) && !context.Items[0].Access.CanWrite())
       {
         return CommandState.Disabled;
       }
       return CommandState.Enabled;
+    }
+
+    protected void StartFieldEditor(ClientPipelineArgs args)
+    {
+      HttpContext current = HttpContext.Current;
+      if (current != null)
+      {
+        Page handler = current.Handler as Page;
+        if (handler != null)
+        {
+          NameValueCollection form = handler.Request.Form;
+          if (form != null)
+          {
+            if (!args.IsPostBack)
+            {
+              SheerResponse.ShowModalDialog(this.GetOptions(args, form).ToUrlString().ToString(), "720", "520", string.Empty, true);
+              args.WaitForPostBack();
+            }
+            else if (args.HasResult)
+            {
+              PageEditFieldEditorOptions.Parse(args.Result).SetPageEditorFieldValues();
+            }
+          }
+        }
+      }
     }
   }
 }
--- src/Sitecore.Support.120811/WebEditResponse.cs
+++ src/Sitecore.Support.120811/WebEditResponse.cs
@@ -1,63 +1,36 @@
-﻿namespace Sitecore.Support.Shell.Applications.WebEdit
+﻿namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
 {
   using Sitecore.Data.Fields;
   using Sitecore.Data.Items;
   using Sitecore.Diagnostics;
+  using Sitecore.Shell.Applications.WebEdit;
   using Sitecore.StringExtensions;
-  using Sitecore.Web;
-  using Sitecore.Web.UI.Sheer;
   using System;
-  using System.Collections.Generic;
 
   public static class WebEditResponse
   {
     internal static void EditFrameUpdateEnd()
     {
-      Commands.Add("Sitecore.PageModes.ChromeManager.handleMessage('chrome:editframe:updateend');");
+      Sitecore.Shell.Applications.WebEdit.WebEditResponse.Commands.Add("Sitecore.PageModes.ChromeManager.handleMessage('chrome:editframe:updateend');");
     }
 
     internal static void EditFrameUpdateStart()
     {
-      Commands.Add("Sitecore.PageModes.ChromeManager.handleMessage('chrome:editframe:updatestart');");
+      Sitecore.Shell.Applications.WebEdit.WebEditResponse.Commands.Add("Sitecore.PageModes.ChromeManager.handleMessage('chrome:editframe:updateend');");
     }
 
-    public static void Eval(string javascript)
-    {
-      Commands.Add(javascript);
-    }
-
-    public static void ExecuteCommands()
-    {
-      foreach (string str in Commands)
-      {
-        SheerResponse.Eval(string.Format("if(!Sitecore.PageModes) {{ window.parent.eval(\"{0}\"); }} else {{ window.eval(\"{0}\"); }}", str));
-      }
-    }
-
-    public static void SetFieldValue(Item item, Field field, string value)
+    internal static void SetFieldValue(Item item, Field field, string value)
     {
       Assert.ArgumentNotNull(item, "item");
       Assert.ArgumentNotNull(field, "field");
       Assert.ArgumentNotNull(value, "value");
       value = value.Replace("\r", string.Empty).Replace("\n", string.Empty).Replace("\t", string.Empty);
       value = value.Replace("\"", "-,scDQuote,-").Replace("'", "-,scSQuote,-");
-      string str = "new Sitecore.ItemUri('{0}', '{1}', '{2}', '{3}')".FormatWith(new object[] { item.ID.ToShortID().ToString(), item.Language.Name, item.Version.ToString(), item.Statistics.Revision });
-      string str2 = "Sitecore.WebEdit.setFieldValue({0}, '{1}', '{2}');".FormatWith(new object[] { str, field.ID.ToShortID().ToString(), value });
-      Commands.Add(str2);
-    }
-
-    public static ICollection<string> Commands
-    {
-      get
-      {
-        ICollection<string> itemsValue = WebUtil.GetItemsValue("WebEditResponse.Commands") as ICollection<string>;
-        if (itemsValue == null)
-        {
-          WebUtil.SetItemsValue("WebEditResponse.Commands", new List<string>());
-          itemsValue = WebUtil.GetItemsValue("WebEditResponse.Commands") as ICollection<string>;
-        }
-        return itemsValue;
-      }
+      object[] parameters = new object[] { item.ID.ToShortID().ToString(), item.Language.Name, item.Version.ToString(), item.Statistics.Revision };
+      string str = "new Sitecore.ItemUri('{0}', '{1}', '{2}', '{3}')".FormatWith(parameters);
+      object[] objArray2 = new object[] { str, field.ID.ToShortID().ToString(), value };
+      string str2 = "Sitecore.WebEdit.setFieldValue({0}, '{1}', '{2}');".FormatWith(objArray2);
+      Sitecore.Shell.Applications.WebEdit.WebEditResponse.Commands.Add(str2);
     }
   }
 }
